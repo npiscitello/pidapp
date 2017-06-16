e58619d2c46845a70998e36f896ca06511692434
NP <nb.piscitello@gmail.com>
2017-06-16 19:09:41 -0400

    restructured data arrays, modularized window margins

--- main.c
+++ main.c
@@ -4,16 +4,22 @@
 #include <string.h>
 
 // window macros
-#define WINDOW_MARGIN 2
-#define NUMWIN 5
-#define COEFF_LINES 5
-#define SETPT_COLS  10
+#define NUMWIN          5
+#define COEFF_LINES     5
+#define SETPT_COLS      10
 
-#define KP 0
-#define KI 1
-#define KD 2
-#define SETPT 3
-#define OUT 4
+/* Horizontal OR vertical space between window edge and window content, in characters. The axis
+ * depends on the shape of the window - horizontal space will be applied in short, wide windows
+ * (coefficient windows) and vertical space will be applied in thin, tall windows (setpoint window).
+ * Windows that are roughly square will have space applied on all sides (output window). */
+#define WINDOW_MARGIN   1 
+
+// Indexes
+#define KP      0
+#define KI      1
+#define KD      2
+#define SETPT   3
+#define OUT     4
 // display is calibrated for coefficient values between 0 (inclusive) and 100 (exclusive)
 const float MIN[NUMWIN] =           {     0,     0,     0,    -5,    -6};
 const float MAX[NUMWIN] =           {    10,  14.5,    29,    20,    21};
@@ -24,9 +30,16 @@ const float DEFAULT_STEP[NUMWIN] =  {     0,     0,     0,     0,     0};
 
 int main() {
   int stdscr_height = 0, stdscr_width = 0;
-  WINDOW* windows[NUMWIN];
-  int height[NUMWIN], width[NUMWIN], step[NUMWIN];
-  float values[NUMWIN], incdec[NUMWIN];
+  struct metadata {
+    WINDOW* hdl;
+    int height;
+    int width;
+  };
+  struct metadata win[NUMWIN]; 
+  // these go outside the struct b/c they're not really window metadata
+  int step[NUMWIN];
+  float value[NUMWIN], incdec[NUMWIN];
+
 
   // start curses and get screen size
   initscr();
@@ -38,41 +51,46 @@ int main() {
   refresh();
 
   // spin up windows
-  windows[KP] = newwin(COEFF_LINES, stdscr_width / 3, 0, 0);
-  windows[KI] = newwin(COEFF_LINES, stdscr_width / 3, 0, stdscr_width / 3);
-  windows[KD] = newwin(COEFF_LINES, 0, 0, 2 * (stdscr_width / 3));
-  windows[SETPT] = newwin(0, SETPT_COLS, COEFF_LINES, 0);
-  windows[OUT] = newwin(0, 0, COEFF_LINES, SETPT_COLS);
+  win[KP].hdl     = newwin(COEFF_LINES, stdscr_width / 3, 0, 0);
+  win[KI].hdl     = newwin(COEFF_LINES, stdscr_width / 3, 0, stdscr_width / 3);
+  win[KD].hdl     = newwin(COEFF_LINES, 0, 0, 2 * (stdscr_width / 3));
+  win[SETPT].hdl  = newwin(0, SETPT_COLS, COEFF_LINES, 0);
+  win[OUT].hdl    = newwin(0, 0, COEFF_LINES, SETPT_COLS);
 
   for( int i = 0; i < NUMWIN; i++ ) {
-    //wborder(windows[i], '|', '|', '-', '-', '.', '.', '\'', '\'');
-    //wrefresh(windows[i]);
-    getmaxyx(windows[i], height[i], width[i]);
-    incdec[i] = (MAX[i] - MIN[i]) / (width[i] - (2 * WINDOW_MARGIN) - 1);
+#ifdef BORDERS
+    wborder(windows[i], '|', '|', '-', '-', '.', '.', '\'', '\'');
+    wrefresh(windows[i]);
+#endif
+    getmaxyx(win[i].hdl, win[i].height, win[i].width);
+    incdec[i] = (MAX[i] - MIN[i]) / (win[i].width - (2 * WINDOW_MARGIN + 2) - 1);
   }
 
   // draw slider borders and labels in K windows
-  mvwprintw(windows[KP], height[KP] / 2 - 1, width[KP] / 2 - 4, "Kp: ");
-  mvwprintw(windows[KP], height[KP] / 2 - 1, 5, "q");
-  mvwprintw(windows[KP], height[KP] / 2 - 1, width[KP] - 6, "w");
+  mvwprintw(win[KP].hdl, win[KP].height / 2 - 1, win[KP].width / 2 - 4, "Kp: ");
+  mvwprintw(win[KP].hdl, win[KP].height / 2 - 1, WINDOW_MARGIN + 4, "q");
+  mvwprintw(win[KP].hdl, win[KP].height / 2 - 1, win[KP].width - (WINDOW_MARGIN + 5), "w");
 
-  mvwprintw(windows[KI], height[KI] / 2 - 1, width[KI] / 2 - 4, "Ki: ");
-  mvwprintw(windows[KI], height[KI] / 2 - 1, 5, "a");
-  mvwprintw(windows[KI], height[KI] / 2 - 1, width[KI] - 6, "s");
+  mvwprintw(win[KI].hdl, win[KI].height / 2 - 1, win[KI].width / 2 - 4, "Ki: ");
+  mvwprintw(win[KI].hdl, win[KI].height / 2 - 1, WINDOW_MARGIN + 4, "a");
+  mvwprintw(win[KI].hdl, win[KI].height / 2 - 1, win[KI].width - (WINDOW_MARGIN + 5), "s");
 
-  mvwprintw(windows[KD], height[KD] / 2 - 1, width[KD] / 2 - 4, "Kd: ");
-  mvwprintw(windows[KD], height[KD] / 2 - 1, 5, "z");
-  mvwprintw(windows[KD], height[KD] / 2 - 1, width[KD] - 6, "x");
+  mvwprintw(win[KD].hdl, win[KD].height / 2 - 1, win[KD].width / 2 - 4, "Kd: ");
+  mvwprintw(win[KD].hdl, win[KD].height / 2 - 1, WINDOW_MARGIN + 4, "z");
+  mvwprintw(win[KD].hdl, win[KD].height / 2 - 1, win[KD].width - (WINDOW_MARGIN + 5), "x");
 
   for( int i = 0; i < NUMWIN; i++ ) {
     if( i == KP || i == KI || i == KD ) {
-      mvwprintw(windows[i], height[i] / 2 - 1, 2, "<-");
-      mvwprintw(windows[i], height[i] / 2, 1, "|");
-      mvwprintw(windows[i], height[i] / 2 - 1, width[i] - 4, "->");
-      mvwprintw(windows[i], height[i] / 2, width[i] - 2, "|");
-      wrefresh(windows[i]);
+      mvwprintw(win[i].hdl, win[i].height / 2 - 1, WINDOW_MARGIN + 1, "<-");
+      mvwprintw(win[i].hdl, win[i].height / 2, WINDOW_MARGIN, "|");
+      mvwprintw(win[i].hdl, win[i].height / 2 - 1, win[i].width - (WINDOW_MARGIN + 1) - 2, "->");
+      mvwprintw(win[i].hdl, win[i].height / 2, win[i].width - (WINDOW_MARGIN + 1), "|");
+      wrefresh(win[i].hdl);
     }
   }
+
+  // draw slider borders and labels in setpoint and output windows
+  //mvwprintw(windows[SETPT], 
 
   // load in default values
   memcpy(step, DEFAULT_STEP, sizeof(DEFAULT_STEP));
@@ -86,7 +104,7 @@ int main() {
         }
         break;
       case 'w':
-        if( step[KP] < width[KP] - (2 * WINDOW_MARGIN) - 1 ) {
+        if( step[KP] < win[KP].width - (2 * (WINDOW_MARGIN + 1)) - 1 ) {
           step[KP]++;
         }
         break;
@@ -97,7 +115,7 @@ int main() {
         }
         break;
       case 's':
-        if( step[KI] < width[KI] - (2 * WINDOW_MARGIN) - 1 ) {
+        if( step[KI] < win[KI].width - (2 * (WINDOW_MARGIN + 1)) - 1 ) {
           step[KI]++;
         }
         break;
@@ -108,7 +126,7 @@ int main() {
         }
         break;
       case 'x':
-        if( step[KD] < width[KD] - (2 * WINDOW_MARGIN) - 1 ) {
+        if( step[KD] < win[KD].width - (2 * (WINDOW_MARGIN + 1)) - 1 ) {
           step[KD]++;
         }
         break;
@@ -129,22 +147,22 @@ int main() {
 
     // fill in sliders and values
     for( int i = 0; i < NUMWIN; i++ ) {
-      values[i] = MIN[i] + (step[i] * incdec[i]);
+      value[i] = MIN[i] + (step[i] * incdec[i]);
       if( i == KP || i == KI || i == KD ) {
 
         // protect from "-0.00"
-        mvwprintw(windows[i], height[i] / 2 - 1, width[i] / 2, "%05.2f", 
-            (values[i] < incdec[i]) ? 0 : values[i]);
+        mvwprintw(win[i].hdl, win[i].height / 2 - 1, win[i].width / 2, "%05.2f", 
+            (value[i] < incdec[i]) ? 0 : value[i]);
 
         // print slider
         for( int j = 0; j < step[i]; j++ ) {
-          mvwprintw(windows[i], height[i] / 2, 2 + j, "=");
+          mvwprintw(win[i].hdl, win[i].height / 2, WINDOW_MARGIN + 1 + j, "=");
         }
-        mvwprintw(windows[i], height[i] / 2, 2 + step[i], "O");
-        for( int j = step[i] + 1; j < width[i] - 4; j++ ) {
-          mvwprintw(windows[i], height[i] / 2, 2 + j, "-");
+        mvwprintw(win[i].hdl, win[i].height / 2, WINDOW_MARGIN + 1 + step[i], "O");
+        for( int j = step[i] + 1; j < win[i].width - (2 * (WINDOW_MARGIN + 1)); j++ ) {
+          mvwprintw(win[i].hdl, win[i].height / 2, WINDOW_MARGIN + 1 + j, "-");
         }
-        wrefresh(windows[i]);
+        wrefresh(win[i].hdl);
       }
     }
   } while( (in = getch()) != ' ' );
